479ae6119f7ffd892f2b89b9d938e69fb175cf01
crtiago <gotia4669>
2022-03-12 10:23:42 -0300

    Adicionando a estrutura inicial ao projeto

---
+++ src/main/java/br/com/crtiago/webservice/market/WebServiceMarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.crtiago.webservice.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crtiago/webservice/market/WebServiceMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crtiago.webservice.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
